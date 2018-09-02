39ede1499b1bc8e333ffce791fab2a1d7ea47eba
朱虹 <zhuhongraid@163.com>
2018-09-02 18:33:43 +0800

    男生发型

--- src/app/select-input/select-input.component.ts
+++ src/app/select-input/select-input.component.ts
@@ -9,6 +9,7 @@ export class SelectInputComponent implements OnInit {
   
   id:any = 'top1';
   clothesArr:any= [];
+  mclothesArr:any=[];
   showArr:any= [];
   public obj: Wear;
   @Output() eventChild1= new EventEmitter();
@@ -152,7 +153,117 @@ export class SelectInputComponent implements OnInit {
         {src:'../../assets/shoes/shoes (14).png',id:'14'},
       ]
     }
-
+    this.mclothesArr = {
+      hairArr:[
+        {src:'../../assets/hair/mhair (1).png',id:'1'},
+        {src:'../../assets/hair/mhair (2).png',id:'2'},
+        {src:'../../assets/hair/mhair (3).png',id:'3'},
+        {src:'../../assets/hair/mhair (4).png',id:'4'},
+        {src:'../../assets/hair/mhair (5).png',id:'5'},
+      ],
+      jewArr:[
+        {src:'../../assets/jew/jew (1).png',id:'1'},
+        {src:'../../assets/jew/jew (2).png',id:'2'},
+        {src:'../../assets/jew/jew (3).png',id:'3'},
+        {src:'../../assets/jew/jew (4).png',id:'4'},
+        {src:'../../assets/jew/jew (5).png',id:'5'},
+        {src:'../../assets/jew/jew (6).png',id:'6'},
+        {src:'../../assets/jew/jew (7).png',id:'7'},
+        {src:'../../assets/jew/jew (8).png',id:'8'},
+        {src:'../../assets/jew/jew (9).png',id:'9'},
+        {src:'../../assets/jew/jew (10).png',id:'11'},
+        {src:'../../assets/jew/jew (11).png',id:'12'},
+        {src:'../../assets/jew/jew (12).png',id:'10'},
+      ],
+      coatArr:[
+        {src:'../../assets/coat/mcoat (1).png',id:'1'},
+        {src:'../../assets/coat/mcoat (2).png',id:'2'},
+        {src:'../../assets/coat/mcoat (3).png',id:'3'},
+        {src:'../../assets/coat/mcoat (4).png',id:'4'},
+        {src:'../../assets/coat/mcoat (5).png',id:'5'},
+        {src:'../../assets/coat/mcoat (6).png',id:'6'},
+        {src:'../../assets/coat/mcoat (7).png',id:'7'},
+        {src:'../../assets/coat/mcoat (8).png',id:'8'},
+        {src:'../../assets/coat/mcoat (9).png',id:'9'},
+        {src:'../../assets/coat/mcoat (10).png',id:'10'},
+        {src:'../../assets/coat/mcoat (11).png',id:'11'},
+        {src:'../../assets/coat/mcoat (12).png',id:'12'},
+        {src:'../../assets/coat/mcoat (13).png',id:'13'},
+        {src:'../../assets/coat/mcoat (14).png',id:'14'},
+        {src:'../../assets/coat/mcoat (15).png',id:'15'},
+        {src:'../../assets/coat/mcoat (16).png',id:'16'},
+        {src:'../../assets/coat/mcoat (17).png',id:'17'},
+        {src:'../../assets/coat/mcoat (18).png',id:'18'},
+        {src:'../../assets/coat/mcoat (19).png',id:'19'},
+        {src:'../../assets/coat/mcoat (20).png',id:'20'},
+        {src:'../../assets/coat/mcoat (21).png',id:'21'},
+        {src:'../../assets/coat/mcoat (22).png',id:'22'},
+        {src:'../../assets/coat/mcoat (23).png',id:'23'},
+        {src:'../../assets/coat/mcoat (24).png',id:'24'},
+        {src:'../../assets/coat/mcoat (25).png',id:'25'},
+        
+      ],
+      suitsArr:[
+        {src:'../../assets/suits/suits (1).png',id:'1',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (2).png',id:'2',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (3).png',id:'3',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (4).png',id:'4',hasShoes:true,hasJew:true},
+        {src:'../../assets/suits/suits (5).png',id:'5',hasShoes:true,hasJew:true},
+        {src:'../../assets/suits/suits (6).png',id:'6',hasShoes:true,hasJew:true},
+        {src:'../../assets/suits/suits (7).png',id:'7',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (8).png',id:'8',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (9).png',id:'9',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (10).png',id:'10',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (11).png',id:'11',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (12).png',id:'12',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (13).png',id:'13',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (14).png',id:'14',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (15).png',id:'15',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (16).png',id:'16',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (17).png',id:'17',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (18).png',id:'18',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (19).png',id:'19',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (20).png',id:'20',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (21).png',id:'21',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (22).png',id:'22',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (23).png',id:'23',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (24).png',id:'24',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (25).png',id:'25',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (26).png',id:'26',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (27).png',id:'27',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (28).png',id:'28',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (29).png',id:'29',hasShoes:false,hasJew:false},
+        {src:'../../assets/suits/suits (30).png',id:'30',hasShoes:false,hasJew:false},
+      ],
+      pantsArr:[
+        {src:'../../assets/pants/pants (1).png',id:'1'},
+        {src:'../../assets/pants/pants (2).png',id:'2'},
+        {src:'../../assets/pants/pants (3).png',id:'3'},
+        {src:'../../assets/pants/pants (4).png',id:'4'},
+        {src:'../../assets/pants/pants (5).png',id:'5'},
+        {src:'../../assets/pants/pants (6).png',id:'6'},
+        {src:'../../assets/pants/pants (7).png',id:'7'},
+        {src:'../../assets/pants/pants (8).png',id:'8'},
+        {src:'../../assets/pants/pants (9).png',id:'9'},
+        {src:'../../assets/pants/pants (10).png',id:'10'},
+      ],
+      shoesArr:[
+        {src:'../../assets/shoes/shoes (1).png',id:'1'},
+        {src:'../../assets/shoes/shoes (2).png',id:'2'},
+        {src:'../../assets/shoes/shoes (3).png',id:'3'},
+        {src:'../../assets/shoes/shoes (4).png',id:'4'},
+        {src:'../../assets/shoes/shoes (5).png',id:'5'},
+        {src:'../../assets/shoes/shoes (6).png',id:'6'},
+        {src:'../../assets/shoes/shoes (7).png',id:'7'},
+        {src:'../../assets/shoes/shoes (8).png',id:'8'},
+        {src:'../../assets/shoes/shoes (9).png',id:'9'},
+        {src:'../../assets/shoes/shoes (10).png',id:'10'},
+        {src:'../../assets/shoes/shoes (11).png',id:'11'},
+        {src:'../../assets/shoes/shoes (12).png',id:'12'},
+        {src:'../../assets/shoes/shoes (13).png',id:'13'},
+        {src:'../../assets/shoes/shoes (14).png',id:'14'},
+      ]
+    }
   }
   select(param){
     this.showArr = param;
